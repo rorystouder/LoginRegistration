f3973c943b48fb46a382e30627849342b5307820
rorystouder <33608831+rorystouder@users.noreply.github.com>
2020-10-01 09:23:35 -0400

    Add files via upload

---
+++ Password Validation/Form1.Designer.cs
@@ -0,0 +1,261 @@
+﻿namespace Login_Registration
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnRegister = new System.Windows.Forms.Button();
+            this.txtConfirmPassword = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtRegisterPassword = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtRegisterName = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtLoginName = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtLoginPassword = new System.Windows.Forms.TextBox();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.lblStatus = new System.Windows.Forms.ToolStripStatusLabel();
+            this.saveFile = new System.Windows.Forms.SaveFileDialog();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnRegister);
+            this.groupBox1.Controls.Add(this.txtConfirmPassword);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.txtRegisterPassword);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.txtRegisterName);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(44, 50);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4);
+            this.groupBox1.Size = new System.Drawing.Size(304, 178);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Register";
+            // 
+            // btnRegister
+            // 
+            this.btnRegister.Location = new System.Drawing.Point(103, 135);
+            this.btnRegister.Margin = new System.Windows.Forms.Padding(4);
+            this.btnRegister.Name = "btnRegister";
+            this.btnRegister.Size = new System.Drawing.Size(100, 28);
+            this.btnRegister.TabIndex = 6;
+            this.btnRegister.Text = "Register";
+            this.btnRegister.UseVisualStyleBackColor = true;
+            this.btnRegister.Click += new System.EventHandler(this.btnRegister_Click);
+            // 
+            // txtConfirmPassword
+            // 
+            this.txtConfirmPassword.Location = new System.Drawing.Point(152, 100);
+            this.txtConfirmPassword.Margin = new System.Windows.Forms.Padding(4);
+            this.txtConfirmPassword.Name = "txtConfirmPassword";
+            this.txtConfirmPassword.Size = new System.Drawing.Size(132, 22);
+            this.txtConfirmPassword.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(19, 105);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(125, 17);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Confirm Password:";
+            // 
+            // txtRegisterPassword
+            // 
+            this.txtRegisterPassword.Location = new System.Drawing.Point(152, 64);
+            this.txtRegisterPassword.Margin = new System.Windows.Forms.Padding(4);
+            this.txtRegisterPassword.Name = "txtRegisterPassword";
+            this.txtRegisterPassword.Size = new System.Drawing.Size(132, 22);
+            this.txtRegisterPassword.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(19, 69);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(73, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Password:";
+            // 
+            // txtRegisterName
+            // 
+            this.txtRegisterName.Location = new System.Drawing.Point(152, 28);
+            this.txtRegisterName.Margin = new System.Windows.Forms.Padding(4);
+            this.txtRegisterName.Name = "txtRegisterName";
+            this.txtRegisterName.Size = new System.Drawing.Size(132, 22);
+            this.txtRegisterName.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(19, 33);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(83, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "User Name:";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.btnLogin);
+            this.groupBox2.Controls.Add(this.label6);
+            this.groupBox2.Controls.Add(this.txtLoginName);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.txtLoginPassword);
+            this.groupBox2.Location = new System.Drawing.Point(44, 250);
+            this.groupBox2.Margin = new System.Windows.Forms.Padding(4);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Padding = new System.Windows.Forms.Padding(4);
+            this.groupBox2.Size = new System.Drawing.Size(304, 126);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Login";
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.Location = new System.Drawing.Point(103, 82);
+            this.btnLogin.Margin = new System.Windows.Forms.Padding(4);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(100, 28);
+            this.btnLogin.TabIndex = 13;
+            this.btnLogin.Text = "Login";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(19, 20);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(83, 17);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "User Name:";
+            // 
+            // txtLoginName
+            // 
+            this.txtLoginName.Location = new System.Drawing.Point(152, 16);
+            this.txtLoginName.Margin = new System.Windows.Forms.Padding(4);
+            this.txtLoginName.Name = "txtLoginName";
+            this.txtLoginName.Size = new System.Drawing.Size(132, 22);
+            this.txtLoginName.TabIndex = 8;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(19, 52);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(73, 17);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Password:";
+            // 
+            // txtLoginPassword
+            // 
+            this.txtLoginPassword.Location = new System.Drawing.Point(152, 49);
+            this.txtLoginPassword.Margin = new System.Windows.Forms.Padding(4);
+            this.txtLoginPassword.Name = "txtLoginPassword";
+            this.txtLoginPassword.Size = new System.Drawing.Size(132, 22);
+            this.txtLoginPassword.TabIndex = 10;
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.lblStatus});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 404);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Padding = new System.Windows.Forms.Padding(1, 0, 19, 0);
+            this.statusStrip1.Size = new System.Drawing.Size(393, 22);
+            this.statusStrip1.TabIndex = 2;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(0, 17);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(393, 426);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "C# Enterprises";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnRegister;
+        private System.Windows.Forms.TextBox txtConfirmPassword;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtRegisterPassword;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtRegisterName;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtLoginName;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtLoginPassword;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel lblStatus;
+        private System.Windows.Forms.SaveFileDialog saveFile;
+    }
+}
---
+++ Password Validation/Form1.cs
@@ -0,0 +1,254 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Login_Registration
+{
+    /* Rory Stouder
+     * Assignment 8
+     * 10/11/17
+     * Password Validation
+     */
+
+    public struct Account
+    {
+        public string name;
+        public string password;
+
+
+        public override string ToString()
+        {
+            return name + ", " + password;
+        }
+    }
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // Global
+        const string ACCOUNT_FILE = "accounts.txt";
+        const string BLANK_ERROR = "field must not be blank!";
+        const int MIN_LENGTH = 8;          // Password's minimum length
+
+        List<Account> allAccounts = new List<Account>();
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                StreamReader accountFile = new StreamReader(ACCOUNT_FILE);
+
+                while (!accountFile.EndOfStream)
+                {
+                    String entireline = accountFile.ReadLine();
+                    String[] tokens = entireline.Split(',');
+
+                    Account newAccount = new Account();
+
+                    newAccount.name = tokens[0];
+                    newAccount.password = tokens[1];
+
+                    allAccounts.Add(newAccount);
+                }
+                accountFile.Close();
+                txtRegisterName.Focus();
+            }
+            catch
+            {
+                lblStatus.Text = "file load did not happen!";
+            }
+        }
+
+        // The NumberUpperCase method accepts a string argument
+        // and returns the number of uppercase letters it contains.
+        private int NumberUpperCase(string str)
+        {
+            int upperCase = 0;  // The number of uppercase letters
+
+            // Count the uppercase characters in str.
+            foreach (char ch in str)
+            {
+                if (char.IsUpper(ch))
+                {
+                    upperCase++;
+                }
+            }
+
+            // Return the number of uppercase characters.
+            return upperCase;
+        }
+
+        // The NumberLowerCase method accepts a string argument
+        // and returns the number of lowercase letters it contains.
+        private int NumberLowerCase(string str)
+        {
+            int lowerCase = 0;  // The number of lowercase letters
+
+            // Count the lowercase characters in str.
+            foreach (char ch in str)
+            {
+                if (char.IsLower(ch))
+                {
+                    lowerCase++;
+                }
+            }
+
+            // Return the number of lowercase characters.
+            return lowerCase;
+        }
+
+        // The NumberDigits method accepts a string argument
+        // and returns the number of numeric digits it contains.
+        private int NumberDigits(string str)
+        {
+            int digits = 0;  // The number of digits
+
+            // Count the digits in str.
+            foreach (char ch in str)
+            {
+                if (char.IsDigit(ch))
+                {
+                    digits++;
+                }
+            }
+
+            // Return the number of digits.
+            return digits;
+        }
+
+        // The Symbol method accepts a string argument
+        // and returns the number of symbols it contains.
+        private int NumberPunctuation(string str)
+        {
+            int symbols = 0; // the number of symbols
+
+            // count the symbols in str.
+            foreach(char ch in str)
+            {
+                if (char.IsPunctuation(ch))
+                {
+                    symbols++;
+                }
+            }
+            // Return the number of symbols
+            return symbols;
+        }
+
+        private void btnRegister_Click(object sender, EventArgs e)
+        {
+            // Get the User Name from the TextBox.
+            string userName = txtRegisterName.Text;
+
+            // Get the password from the TextBox.
+            string password = txtRegisterPassword.Text;
+            string confirmPassword = txtConfirmPassword.Text;
+
+            if (userName.Length == 0 &&
+                password.Length == 0 &&
+                confirmPassword.Length == 0)
+            {
+                lblStatus.Text = ("the User Name " + BLANK_ERROR);
+                txtRegisterName.Focus();
+                return;
+            }
+            else if (userName.Contains(',') &&
+                password.Contains(',') &&
+                confirmPassword.Contains(','))
+            {
+                lblStatus.Text = "You can not use a comma in any field!";
+                txtRegisterName.Focus();
+                return;
+            }
+
+            // Validate the password.
+            if (password.Length >= MIN_LENGTH &&
+                NumberUpperCase(password) >= 1 &&
+                NumberLowerCase(password) >= 1 &&
+                NumberDigits(password) >= 1 &&
+                NumberPunctuation(password) >= 1)
+            {
+                if (password.Equals(txtConfirmPassword.Text))
+                {
+                    Account newAccount = new Account();
+
+                    newAccount.name = txtRegisterName.Text;
+                    newAccount.password = txtRegisterPassword.Text;
+
+                    if (allAccounts.Contains(newAccount))
+                    {
+                        lblStatus.Text = "That username has already taken, pick another.";
+                        txtRegisterName.Focus();
+                        return;
+                    }
+                    else
+                    {
+                        // save to list
+                        allAccounts.Add(newAccount);
+                        lblStatus.Text = "New Account Confirmed, Welcome to C#";
+
+                        // append file.
+                        StreamWriter outputFile;
+
+                        saveFile.FileName = ACCOUNT_FILE;
+                        saveFile.Filter = "Plain Text (*.txt)| *.txt | Word (*.doc)| *.doc | Open Text (*.odt)| *.odt | Rich Text (*)| *.rtf ";
+
+                        try
+                        {
+                            outputFile = File.CreateText(saveFile.FileName);
+
+                            foreach (Account obj in allAccounts)
+                            {
+                                outputFile.WriteLine((obj.name) + "," + (obj.password));
+                            }
+                            outputFile.Close();
+                        }
+                        catch
+                        {
+                            lblStatus.Text = "file write did not happen!";
+                        }
+
+                        // Clear all fields
+                        txtRegisterName.Text = string.Empty;
+                        txtRegisterPassword.Text = string.Empty;
+                        txtConfirmPassword.Text = string.Empty;
+                    }
+                }
+            }
+            else
+            {
+                lblStatus.Text = ("The password does not meet " +
+                    "the requirements.");
+            }
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            foreach (Account account in allAccounts)
+            {
+                if (account.name == txtLoginName.Text &&
+                    account.password == txtLoginPassword.Text)
+                {
+                    lblStatus.Text = "Welcome to the system, " + account.name;
+                    txtLoginName.Text = string.Empty;
+                    txtLoginPassword.Text = string.Empty;
+                    return;
+                }                 
+            }
+
+            lblStatus.Text = "Login credentials are not correct";
+
+            txtLoginPassword.Text = string.Empty;
+            txtLoginName.Text = string.Empty;
+        }
+    }
+}
